ea574e220929d96253016fa9d4063e6d90205dbd
zsordo <zinebsordo@hotmail.fr>
2022-08-19 11:40:54 -0400

    removed logs cuda

--- model_training.py
+++ model_training.py
@@ -203,12 +203,6 @@ def run_experiment(args):
     if torch.cuda.is_available():
         print("Found CUDA device, running job on GPU.")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    if device.type == 'cuda':
-        print(torch.cuda.get_device_name(0))
-        print('Memory Usage:')
-        print('Allocated:', round(torch.cuda.memory_allocated(0) / 1024 ** 3, 1), 'GB')
-        print('Cached:   ', round(torch.cuda.memory_reserved(0) / 1024 ** 3, 1), 'GB')
-
     datamodule = get_data(args)
     model = get_model(args, device)
     if args.mode == "train":
