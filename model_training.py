2d6d877eb89e855f841cdc79546a0942c70370f5
zsordo <zinebsordo@hotmail.fr>
2022-08-21 12:03:04 -0400

    changed to csv logs only

--- model_training.py
+++ model_training.py
@@ -8,7 +8,7 @@ import os
 import pytorch_lightning as pl
 import torch
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, LearningRateMonitor
-from pytorch_lightning.loggers import CSVLogger, WandbLogger
+from pytorch_lightning.loggers import CSVLogger#, WandbLogger
 from pathlib import Path
 import fire
 
@@ -138,7 +138,7 @@ def train_model(
         os.makedirs(str(model_dir))
 
     csv_logger = CSVLogger(save_dir=log_dir, name=f"train-{args.n_seed}", version=f"{args.n_seed}")
-    wandb_logger = WandbLogger(name=f"{args.data_space}-{args.n_seed}")
+    #wandb_logger = WandbLogger(name=f"{args.data_space}-{args.n_seed}")
     lr_monitor = LearningRateMonitor(logging_interval='step')
     model_checkpoint = ModelCheckpoint(monitor='val_auc_mean', dirpath=model_dir, filename="{epoch:02d}-{val_auc_mean:.2f}" ,save_top_k=1, mode='max')
     early_stop_callback = EarlyStopping(monitor='val_auc_mean', patience=10, mode='max')
@@ -149,8 +149,8 @@ def train_model(
         strategy="ddp",
         max_epochs=args.n_epochs,
         #logger=wandb_logger,
-        #logger=csv_logger,
-        logger=[wandb_logger, csv_logger],
+        logger=csv_logger,
+        #logger=[wandb_logger, csv_logger],
         callbacks=[model_checkpoint, early_stop_callback, lr_monitor],
         auto_lr_find=True,
     )
@@ -162,9 +162,9 @@ def train_model(
         devices=3,
         strategy="ddp",
         max_epochs=args.n_epochs,
-        logger=[wandb_logger, csv_logger],
+        #logger=[wandb_logger, csv_logger],
         #logger=wandb_logger,
-        #logger=csv_logger,
+        logger=csv_logger,
         callbacks=[model_checkpoint, early_stop_callback, lr_monitor],
     )
     trainer.fit(model, datamodule)
