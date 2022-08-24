9ca847dc06dd13d4d8b33c95aefb63505bfd9d6a
zsordo <zinebsordo@hotmail.fr>
2022-08-24 11:50:18 -0400

    added forgotten shape in log

--- data_processing.py
+++ data_processing.py
@@ -187,7 +187,7 @@ class KneeDataset(MultiDataset):
                 "dataset": info.dataset,
                 "location": info.location,
             }
-            print("in parameters the kspace shape is {}".format(parameters['sc_kspace']))
+            print("in parameters the kspace shape is {}".format(parameters['sc_kspace']).shape)
             if self.label_type == "knee_multilabel":
                 parameters["label"] = self.parse_multilabel(info.labels)
             elif self.label_type == "knee":
