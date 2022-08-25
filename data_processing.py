9d557b7e2b528cba970956cdedfe2b993ca6fa0b
zsordo <zinebsordo@hotmail.fr>
2022-08-25 12:37:07 -0400

    added train_sampler

--- data_processing.py
+++ data_processing.py
@@ -271,7 +271,7 @@ class KneeDataModule(pl.LightningDataModule):
 
     def train_dataloader(self) -> DataLoader:
         # return DataLoader(self.train_dataset, batch_size=self.batch_size,  sampler=self.train_sampler, num_workers=self.num_workers,)
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers,)
+        return DataLoader(self.train_dataset, batch_size=self.batch_size,  sampler=self.train_sampler, num_workers=self.num_workers,)
 
     def val_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
         return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
