a78112cae06c315b429fb48d9aad304f8300dcf7
zsordo <zinebsordo@hotmail.fr>
2022-09-01 17:06:24 -0500

    modified modReLU to support adam optimizer

--- complex_activation_functions.py
+++ complex_activation_functions.py
@@ -15,7 +15,7 @@ def modReLU(input, bias):
 
     a, b = input.real, input.imag
     input_mag = input.abs()
-    mask = ((input_mag + bias) >= 0).float() * (1 + bias / input_mag)
+    mask = ((input_mag + bias) > 0).float() * (1 + bias / input_mag)
     real = mask * a
     imag = mask * b
     result = real.type(torch.complex64) + 1j*imag.type(torch.complex64)
