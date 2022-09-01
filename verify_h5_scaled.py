0e0f1288c7d7d5123687163b4c0d11b01da5b0a8
zsordo <zinebsordo@hotmail.fr>
2022-09-01 16:00:20 -0500

    added tqdm

--- verify_h5_scaled.py
+++ verify_h5_scaled.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import h5py
-
+import tqdm
 
 if __name__ == '__main__':
 
     file_path = "./metadata_knee.csv"
     df = pd.read_csv(file_path)
 
-    for i in range(len(df)):
+    for i in tqdm.tqdm(range(len(df))):
         file = h5py.File(df.location.iloc[i])
         if 'sc_kspace_scaled' in file.keys():
             continue
